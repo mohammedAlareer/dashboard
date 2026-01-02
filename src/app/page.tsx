2636193d0ea2175afb27ec085c466d2813e77fa3
Mohammed <you@example.com>
2026-01-02 21:14:23 +0200

    create Bar Chart component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import AppBarChart from "@/components/AppBarChart";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-    
+   <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+    <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+      <AppBarChart/>
+    </div>
+    <div>test</div>
+    <div>test</div>
+    <div>test</div>
+    <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"></div>
+    <div>test</div>
    </div>
   );
 }
